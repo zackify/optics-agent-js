e714834b47efbdaf64f3292d948d36070238730f
Jonas Helfer <jonas@helfer.email>
2016-09-29 21:16:41 -0700

    first step normalizing queries

--- src/Normalize.test.js
+++ src/Normalize.test.js
@@ -1,18 +1,51 @@
 import { assert } from 'chai';
+import gql from 'graphql-tag';
 
 import { normalizeQuery } from './Normalize';
 
 
 const testQueries = [
-  // XXX construct info operations
-  // [
-    
-  // ]
+  [
+    'basic test',
+    gql`query Foo {
+      user(name: "hello") {
+        ... Bar
+        tz
+        aliased: name
+      }
+    }
+    fragment Baz on User {
+      asd
+    }
+    fragment Bar on User {
+      age
+      ...Nested
+    }
+    fragment Nested on User {
+      blah
+    }`,
+    'query Foo{user(name:"") {name tz ...Bar}}fragment Bar on User{age ...Nested}fragment Nested on User{blah}',
+  ],
 ];
 
 describe('normalizeQuery', () => {
-  testQueries.map(([info, outString],i) => {
-    it(''+i, () =>
-       assert.equal(outString, normalizeQuery(info), 'normalize'));
+  testQueries.map(([testName, inputDocument, outString],i) => {
+    it(testName, () => {
+      const fragments = {};
+      let operation = null;
+      inputDocument.definitions.forEach( def => {
+        if (def.kind === 'OperationDefinition'){
+          operation = def;
+        }
+        if (def.kind === 'FragmentDefinition'){
+          fragments[def.name.value] = def;
+        }
+      });
+      const fakeInfo = {
+        operation,
+        fragments,
+      };
+      assert.equal(normalizeQuery(fakeInfo), outString, 'normalize');
+    });
   });
 });
